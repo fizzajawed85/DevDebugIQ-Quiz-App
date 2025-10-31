b660d62b2c2e66f45ed86051696d391751d76cd5
fizzajawed85 <angelfizza85@gmail.com>
2025-11-01 04:50:17 +0500

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- makes asset paths relative for production
   server: {
     port: 5173,
     proxy: {
@@ -10,3 +11,4 @@ export default defineConfig({
     }
   }
 })
+
